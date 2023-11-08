fee06f1fda7bab9fc2079e7ca02d486ae3c7c524
moshiur26 <moshiurrahmanb11@gmail.com>
2023-11-09 01:57:56 +0600

    added sortings

---
+++ sorting/merge_sort.py
@@ -0,0 +1,34 @@
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    mid = len(arr) // 2
+    left = arr[:mid]
+    right = arr[mid:]
+
+    left = merge_sort(left)
+    right = merge_sort(right)
+
+    return merge(left, right)
+
+
+def merge(left, right):
+    result = []
+    while left and right:
+        if left[0] <= right[0]:
+            result.append(left.pop(0))
+        else:
+            result.append(right.pop(0))
+
+    while left:
+        result.append(left.pop(0))
+    while right:
+        result.append(right.pop(0))
+
+    return result
+
+
+# Example usage:
+arr = [12, 4, 7, 2, 10, 15, 3, 9]
+sorted_arr = merge_sort(arr)
+print("Sorted Array:", sorted_arr)
